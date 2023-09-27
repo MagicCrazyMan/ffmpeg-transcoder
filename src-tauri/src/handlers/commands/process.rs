b516f0aef6a7af17e695ce6e0a40d8db85f84d14
MagicCrazyMan <350088648@qq.com>
2023-09-27 14:24:17 +0800

    wip

--- src-tauri/src/handlers/commands/process.rs
+++ src-tauri/src/handlers/commands/process.rs
@@ -15,40 +15,21 @@ macro_rules! with_default_args {
     };
 }
 
-trait IntoResult<T> {
-    fn into_result(self) -> Result<T, Error>;
-}
+async fn invoke(program: &str, args: &[&str]) -> Result<Output, Error> {
+    let output = Command::new(program).args(args).output().await;
 
-impl<T> IntoResult<T> for std::io::Result<T> {
-    fn into_result(self) -> Result<T, Error> {
-        match self {
-            Ok(v) => Ok(v),
-            Err(e) => match e.kind() {
-                std::io::ErrorKind::NotFound => Err(Error::new(ErrorKind::FFmpegNotFound)),
-                _ => Err(Error::from_raw_error(e, ErrorKind::Internal)),
-            },
-        }
+    match output {
+        Ok(output) => Ok(output),
+        Err(err) => Err(Error::from_raw_error(err, ErrorKind::Internal)),
     }
 }
 
 /// Invokes ffmpeg in child process and returns output result after process end.
 pub async fn invoke_ffmpeg(ffmpeg: &str, args: &[&str]) -> Result<Output, Error> {
-    let output = Command::new(ffmpeg)
-        .args(args)
-        .output()
-        .await
-        .into_result()?;
-
-    Ok(output)
+    invoke(ffmpeg, args).await
 }
 
 /// Invokes ffprobe in child process and returns output result after process end.
 pub async fn invoke_ffprobe(ffprobe: &str, args: &[&str]) -> Result<Output, Error> {
-    let output = Command::new(ffprobe)
-        .args(args)
-        .output()
-        .await
-        .into_result()?;
-
-    Ok(output)
+    invoke(ffprobe, args).await
 }
