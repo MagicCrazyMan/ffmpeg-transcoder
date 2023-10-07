8d01d17a95e8c68c3b8fd8c0b6b1f9d4f09e6f5d
MagicCrazyMan <350088648@qq.com>
2023-10-07 09:58:08 +0800

    wip

--- src/routes/Queue.tsx
+++ src/routes/Queue.tsx
@@ -191,39 +191,31 @@ const Operations = ({ task, onDetails }: { task: Task; onDetails: () => void }) 
 };
 
 const Progress = ({ task }: { task: Task }) => {
-  const progress = useMemo(() => {
-    if (task.message) {
-      switch (task.message.type) {
-        case TaskState.Running: {
-          const total = task.message.total_duration;
-          const output = (task.message.output_time_ms ?? 0) / 1000000;
-          const percent = (output / total) * 100;
-          return (
-            <ProgressBar
-              animation
-              percent={percent}
-              strokeWidth={20}
-              formatText={(percent) => `${percent.toFixed(2)}%`}
-            />
-          );
-        }
-        case TaskState.Pausing:
-          return <IconPauseCircle fontSize="24px" style={{ color: "orange" }} />;
-        case TaskState.Stopped:
-          return <IconStop fontSize="24px" style={{ color: "red" }} />;
-        case TaskState.Finished:
-          return <IconCheckCircle fontSize="24px" style={{ color: "green" }} />;
+  if (task.message) {
+    switch (task.message.type) {
+      case TaskState.Running: {
+        const total = task.message.total_duration;
+        const output = (task.message.output_time_ms ?? 0) / 1000000;
+        const percent = (output / total) * 100;
+        return (
+          <ProgressBar
+            animation
+            percent={percent}
+            strokeWidth={20}
+            formatText={(percent) => `${percent.toFixed(2)}%`}
+          />
+        );
       }
-    } else {
-      return <IconMore fontSize="24px" style={{ color: "#86909C" }} />;
+      case TaskState.Pausing:
+        return <IconPauseCircle fontSize="24px" style={{ color: "orange" }} />;
+      case TaskState.Stopped:
+        return <IconStop fontSize="24px" style={{ color: "red" }} />;
+      case TaskState.Finished:
+        return <IconCheckCircle fontSize="24px" style={{ color: "green" }} />;
     }
-  }, [task]);
-
-  return (
-    <>
-      <div>{progress}</div>
-    </>
-  );
+  } else {
+    return <IconMore fontSize="24px" style={{ color: "#86909C" }} />;
+  }
 };
 
 const DetailsContent = ({ taskId }: { taskId?: string }) => {
